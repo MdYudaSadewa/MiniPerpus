c1a7daec7ceef67dc530bc144a76e048073647d1
MdYudaSadewa <51825022+MdYudaSadewa@users.noreply.github.com>
2019-06-14 21:17:46 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Menu from './Miniperpus/Menu'
+import Home from './Miniperpus/home'
+export default class App extends React.Component {
+    render() {
+    return (
+     <Menu/>
+    );
+  }
+}
+
+
